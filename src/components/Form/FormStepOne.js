7f4273ada7fd927900cc39552d281e31eeebc2ce
nadgobxe <bogdan.burcea22@my.northampton.ac.uk>
2024-03-01 12:59:33 +0000

    Refactor FormStepOne component

--- src/components/Form/FormStepOne.js
+++ src/components/Form/FormStepOne.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Input, Select, SelectItem } from "@nextui-org/react";
-import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 
 
 export default function FormStepOne() {
@@ -37,27 +36,25 @@ export default function FormStepOne() {
     }
 
 
-    const handlePlaceSelect = (address, inputType) => {
-        if (inputType === 'pick-up') {
-            setPickupAddress(address);
-        } else if (inputType === 'delivery') {
-            setDeliveryAddress(address);
-        }
-    };
+
 
     return (
         <div className="flex w-full flex-wrap items-end mt-10 md:flex-nowrap mb-6 md:mb-0 gap-4">
-            <GooglePlacesAutocomplete 
-                apiKey={process.env.REACT_APP_GOOGLE_API_KEY}
-                selectProps={{
-                    value: pickupAddress,
-                    onChange: (address) => handlePlaceSelect(address, 'pick-up'),
-                    placeholder: 'Pick-Up Address',
-                    className: "w-full md:w-auto text-black font-raleway font-semibold next-ui-input bg-white rounded-md"
-                }}
-            />
-        
-            {console.log(deliveryAddress)}
+            <Input onChange={handlePostCode}/>
+            {deliveryAddress ? (
+                <Select placeholder="Delivery Address">
+                    {console.log(deliveryAddress)}
+                    {deliveryAddress &&
+                        deliveryAddress.map((address, index) => (
+                            <SelectItem
+                                key={index}
+                                value={address}
+                                label={address}
+                            > {address}</SelectItem>
+                        ))}
+                </Select>
+            ) : null}
+
             <Button
                 className="bg-[#2EBBB6] w-full md:w-auto text-white font-raleway font-semiboldnp"
                 color="primary"
@@ -66,16 +63,7 @@ export default function FormStepOne() {
                 Get A Quote
             </Button>
 
-            <Input onChange={handlePostCode}/>
-            {deliveryAddress ? <Select placeholder="Delivery Address">
-            {deliveryAddress && deliveryAddress.map((address, index) => (
-                // Code inside the map function
-         
-                    <SelectItem key={index} value={address} label={address} className='text-black'/>
-                 
-            ))}
-              </Select> :
-              null}  
+            
         </div>
     );
 }
