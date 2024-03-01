992032dbcb5b7bc93f17ce5c2ec35f0dcdb497f9
nadgobxe <bogdan.burcea22@my.northampton.ac.uk>
2024-03-01 12:42:39 +0000

    Refactor FormStepOne component and update index.html

--- src/components/Form/FormStepOne.js
+++ src/components/Form/FormStepOne.js
@@ -1,11 +1,40 @@
 import React, { useState, useEffect } from 'react';
-import { Button } from "@nextui-org/react";
+import { Button, Input, Select, SelectItem } from "@nextui-org/react";
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 
 
 export default function FormStepOne() {
     const [pickupAddress, setPickupAddress] = useState(null);
-    const [deliveryAddress, setDeliveryAddress] = useState(null);
+    const [deliveryAddress, setDeliveryAddress] = useState();
+    const [showSelectDelivery, setShowSelectDelivery] = useState(false);
+
+    function findPostalCode(query) {
+        const apiKey = 'uJ8qNXP3RGTWXkNJItNvWA1MqLww2NZA'; // Be cautious with API keys in frontend code
+        const url = `https://api.os.uk/search/places/v1/find?query=${encodeURIComponent(query)}&key=${apiKey}`;
+    
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                // Processing the data to extract postal codes
+                const postalCodes = data.results.map(result => {
+                    // Depending on the structure of the API response, this path might need adjustment
+                    return result.DPA.ADDRESS;
+                });
+                setDeliveryAddress(postalCodes);
+                console.log(postalCodes);
+            })
+            .catch(error => {
+                console.error('Error fetching postal code:', error);
+            });
+    }
+
+    const handlePostCode = (e) => { 
+        e.preventDefault();
+        if (e.target.value.length > 3) {
+            findPostalCode(e.target.value);
+        }
+        
+    }
 
 
     const handlePlaceSelect = (address, inputType) => {
@@ -27,6 +56,8 @@ export default function FormStepOne() {
                     className: "w-full md:w-auto text-black font-raleway font-semibold next-ui-input bg-white rounded-md"
                 }}
             />
+        
+            {console.log(deliveryAddress)}
             <Button
                 className="bg-[#2EBBB6] w-full md:w-auto text-white font-raleway font-semiboldnp"
                 color="primary"
@@ -34,6 +65,17 @@ export default function FormStepOne() {
             >
                 Get A Quote
             </Button>
+
+            <Input onChange={handlePostCode}/>
+            {deliveryAddress ? <Select placeholder="Delivery Address">
+            {deliveryAddress && deliveryAddress.map((address, index) => (
+                // Code inside the map function
+         
+                    <SelectItem key={index} value={address} label={address} className='text-black'/>
+                 
+            ))}
+              </Select> :
+              null}  
         </div>
     );
 }
